bd2b715e540b7881a5c775a0a3125ea378f74472
Peter deHaan <peter@deseloper.com>
2023-11-30 17:09:28 -0800

    Query more fields

--- index.js
+++ index.js
@@ -7,54 +7,61 @@ const AUTH_TOKEN = jiraAuthToken(JIRA_EMAIL, JIRA_TOKEN);
 
 const bodyData = `{
   "expand": [
-    "names",
-    "schema",
-    "operations"
+    "schema"
   ],
   "fields": [
     "summary",
+    "link",
+    "resolution",
     "status",
-    "assignee"
+    "created",
+    "updated",
+    "components",
+    "severity",
+    "labels",
+    "assignee",
+    "reporter"
   ],
   "fieldsByKeys": false,
-  "jql": "project = FXA",
-  "maxResults": 5,
+  "jql": "project = FXA AND issuetype in (Bug, Task) AND resolution = Unresolved",
+  "maxResults": 100,
   "startAt": 0
 }`;
 
 try {
   const res = await fetchJira("/rest/api/3/search", bodyData);
-  console.log(res);
+  console.log(JSON.stringify(res, null, 2));
 } catch (err) {
   console.error(err);
 }
 
 async function fetchJira(path, bodyData) {
-  const res = await fetch(
-    new URL(path, JIRA_SERVER).href,
-    {
-      method: "POST",
-      headers: {
-        Authorization: AUTH_TOKEN,
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: bodyData,
-    }
-  );
+  const res = await fetch(new URL(path, JIRA_SERVER).href, {
+    method: "POST",
+    headers: {
+      Authorization: AUTH_TOKEN,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: bodyData,
+  });
+
+  // console.log(res.headers);
+
   const data = await res.json();
-  data.$meta = {
-    status: res.status,
-    statusText: res.statusText,
-  };
+
   if (res.statusText !== "OK") {
-    const err = new Error(data.message);
+    const err = new Error(data.errorMessages[0]);
     err.error = data;
     err.status = res.status;
     err.statusText = res.statusText;
     process.exitCode = 1;
     throw err;
   }
+  data.$meta = {
+    status: res.status,
+    statusText: res.statusText,
+  };
   return data;
 }
 
